31f2e49634b24e1b75f12c907d14e4f51c20e18e
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-03-08 15:47:12 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client';
-import { StrictMode } from 'react';
+import React, { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';

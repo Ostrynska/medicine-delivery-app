e5e0c49f439d6a9906af98ea43b311626f6db2b8
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-03-08 16:26:44 +0200

    Update Home.jsx

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -5,7 +5,6 @@ import { Outlet } from 'react-router-dom';
 import { fetchAllShops } from '../../redux/shops/operations';
 
 import { HomeWrapp, ShopsWrapp, Link } from './Home.styled';
-import { SharedLayout } from '../../components/SharedLayout/SharedLayout';
 
 const Home = () => {
  const dispatch = useDispatch();
@@ -21,35 +20,33 @@ const Home = () => {
  };
 
  return (
-  <SharedLayout>
-   <main>
-    <h1 hidden>Medicine Delivery Shops</h1>
-    <section>
-     <HomeWrapp
-      style={{
-       backgroundImage: backgroundLoaded
-        ? 'none'
-        : "url('https://png.pngtree.com/png-clipart/20230927/original/pngtree-medicine-ordering-mobile-app-concept-of-healthcare-drugstore-and-online-pharmacy-png-image_13159766.png')",
-       backgroundSize: '699px 588px',
-       backgroundRepeat: 'no-repeat',
-       backgroundPosition: 'right',
-      }}
-     >
-      <ShopsWrapp>
-       <h2>Shops</h2>
-       <ul>
-        {shops.map(({ _id, name }) => (
-         <Link key={_id} to={`/shops/${_id}`} onClick={handleLinkClick}>
-          {name}
-         </Link>
-        ))}
-       </ul>
-      </ShopsWrapp>
-      <Outlet />
-     </HomeWrapp>
-    </section>
-   </main>
-  </SharedLayout>
+  <main>
+   <h1 hidden>Medicine Delivery Shops</h1>
+   <section>
+    <HomeWrapp
+     style={{
+      backgroundImage: backgroundLoaded
+       ? 'none'
+       : "url('https://png.pngtree.com/png-clipart/20230927/original/pngtree-medicine-ordering-mobile-app-concept-of-healthcare-drugstore-and-online-pharmacy-png-image_13159766.png')",
+      backgroundSize: '699px 588px',
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'right',
+     }}
+    >
+     <ShopsWrapp>
+      <h2>Shops</h2>
+      <ul>
+       {shops.map(({ _id, name }) => (
+        <Link key={_id} to={`/shops/${_id}`} onClick={handleLinkClick}>
+         {name}
+        </Link>
+       ))}
+      </ul>
+     </ShopsWrapp>
+     <Outlet />
+    </HomeWrapp>
+   </section>
+  </main>
  );
 };
 

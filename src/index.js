a843cf09e9df96e9befca87423324651c3e256ad
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-03-11 12:21:13 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import React, { StrictMode } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
@@ -10,13 +9,13 @@ import { App } from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- //  <StrictMode>
- <Provider store={store}>
-  <PersistGate loading={null} persistor={persistor}>
-   <BrowserRouter basename="/medicine-delivery-app/">
-    <App />
-   </BrowserRouter>
-  </PersistGate>
- </Provider>
- //  </StrictMode>
+ <React.StrictMode>
+  <Provider store={store}>
+   <PersistGate loading={null} persistor={persistor}>
+    <BrowserRouter basename="/medicine-delivery-app/">
+     <App />
+    </BrowserRouter>
+   </PersistGate>
+  </Provider>
+ </React.StrictMode>
 );

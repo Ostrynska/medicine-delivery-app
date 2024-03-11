f77688b39a2f7bd3a9531ac0ff7ef4723bb2ca77
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-03-11 13:32:11 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -10,15 +10,15 @@ import Loader from './components/Loader/Loader';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
- //  <React.StrictMode>
- <Provider store={store}>
-  <PersistGate loading={null} persistor={persistor}>
-   <BrowserRouter basename="/medicine-delivery-app">
-    <Suspense fallback={<Loader />}>
-     <App />
-    </Suspense>
-   </BrowserRouter>
-  </PersistGate>
- </Provider>
- //  </React.StrictMode>
+ <React.StrictMode>
+  <Provider store={store}>
+   <PersistGate loading={null} persistor={persistor}>
+    <BrowserRouter basename="/medicine-delivery-app">
+     <Suspense fallback={<Loader />}>
+      <App />
+     </Suspense>
+    </BrowserRouter>
+   </PersistGate>
+  </Provider>
+ </React.StrictMode>
 );

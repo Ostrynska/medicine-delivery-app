e3420e71fd9ca57db2bbcd7eecbbe98651cde931
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-03-11 15:13:42 +0200

    Update SharedLayout.jsx

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,28 +1,22 @@
 import React from 'react';
-import { Outlet } from 'react-router-dom';
 
-import Footer from '../Footer/Footer';
-import { Container, Header, Logo, Link } from './SharedLayout.styled';
+import { Header, Logo, Link } from './SharedLayout.styled';
 
 export const SharedLayout = () => {
  return (
-  <Container>
-   <Header>
-    <Logo to="/shops">
-     <span role="img" aria-label="medicine icon">
-      💊
-     </span>
-     Medicine Delivery
-    </Logo>
-    <nav>
-     <Link to="/shops" end>
-      Shop
-     </Link>
-     <Link to="/cart">Shopping Cart</Link>
-    </nav>
-   </Header>
-   <Outlet />
-   <Footer />
-  </Container>
+  <Header>
+   <Logo to="/shops">
+    <span role="img" aria-label="medicine icon">
+     💊
+    </span>
+    Medicine Delivery
+   </Logo>
+   <nav>
+    <Link to="/shops" end>
+     Shop
+    </Link>
+    <Link to="/cart">Shopping Cart</Link>
+   </nav>
+  </Header>
  );
 };

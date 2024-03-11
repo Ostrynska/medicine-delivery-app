3502bb14e496bcbe96ceaa5d8a16ff2d99ae8d9c
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-03-11 11:37:37 +0200

    Revert "Revert "Update App.jsx""
    This reverts commit 6a0dfa53c7bae2d53082a40e93f9db53580359ad.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,8 @@
-// import React, { useEffect, useState, lazy } from 'react';
-import React, { lazy } from 'react';
+import React, { useEffect, useState, lazy, Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 import { SharedLayout } from './SharedLayout/SharedLayout';
+import Loader from './Loader/Loader';
 import Footer from './Footer/Footer';
-// import Loader from './Loader/Loader';
 
 import '../styles/index.css';
 
@@ -12,34 +11,36 @@ const Cart = lazy(() => import('../pages/CartPage/Cart'));
 const DrugsList = lazy(() => import('./DrugsList/DrugsList'));
 
 export const App = () => {
- //  const [loading, setLoading] = useState(true);
+ const [loading, setLoading] = useState(true);
 
- //  useEffect(() => {
- //   const delay = 3000;
- //   const timer = setTimeout(() => {
- //    setLoading(false);
- //   }, delay);
- //   return () => clearTimeout(timer);
- //  }, []);
+ useEffect(() => {
+  const delay = 3000;
+  const timer = setTimeout(() => {
+   setLoading(false);
+  }, delay);
+  return () => clearTimeout(timer);
+ }, []);
  return (
-  //   <Suspense fallback={<Loader />}>
-  //    {loading ? (
-  //     <Loader />
-  //    ) : (
-  <>
-   <SharedLayout />
-   <main>
-    <Routes>
-     <Route path="/shops" element={<Home />}>
-      <Route path=":id" element={<DrugsList />} />
-     </Route>
-     <Route path="/cart" element={<Cart />} />
-     <Route path="*" element={<p>Path not resolved</p>} />
-    </Routes>
-   </main>
-   <Footer />
-  </>
-  //    )}
-  //   </Suspense>
+  <Suspense fallback={<Loader />}>
+   {loading ? (
+    <Loader />
+   ) : (
+    <>
+     <SharedLayout />
+     <main>
+      <Routes>
+       <Route path="/" element={<SharedLayout />}>
+        <Route index path="/shops" element={<Home />}>
+         <Route path=":id" element={<DrugsList />} />
+        </Route>
+        <Route path="/cart" element={<Cart />} />
+        <Route path="*" element={<p>Path not resolved</p>} />
+       </Route>
+      </Routes>
+     </main>
+     <Footer />
+    </>
+   )}
+  </Suspense>
  );
 };

58b24d91cf51fac5f071b4d07f7e9f8392516299
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-03-11 16:00:22 +0200

    Update Header.styled.jsx

--- src/components/Header/Header.styled.jsx
+++ src/components/Header/Header.styled.jsx
@@ -1,21 +1,6 @@
 import styled from 'styled-components';
 import { NavLink } from 'react-router-dom';
 
-export const Container = styled.div`
- max-width: 360px;
- margin: 0 auto;
- padding: 0 16px;
- @media screen and (min-width: 576px) {
-  max-width: 540px;
- }
- @media screen and (min-width: 768px) {
-  max-width: 720px;
- }
- @media screen and (min-width: 1200px) {
-  max-width: 1200px;
- }
-`;
-
 export const HeaderWrapp = styled.header`
  display: flex;
  align-items: center;

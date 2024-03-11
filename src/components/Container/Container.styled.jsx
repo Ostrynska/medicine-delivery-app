9586c3eca2f8d9c4a4f15f366fc7b67baa82028d
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-03-11 16:00:19 +0200

    Create Container.styled.jsx

---
+++ src/components/Container/Container.styled.jsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+ max-width: 360px;
+ margin: 0 auto;
+ padding: 0 16px;
+ @media screen and (min-width: 576px) {
+  max-width: 540px;
+ }
+ @media screen and (min-width: 768px) {
+  max-width: 720px;
+ }
+ @media screen and (min-width: 1200px) {
+  max-width: 1200px;
+ }
+`;

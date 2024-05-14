d12140213a95021d62e512bcef77bf37f17e569e
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2024-05-14 18:07:00 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ root.render(
  <React.StrictMode>
   <Provider store={store}>
    <PersistGate loading={null} persistor={persistor}>
-    <BrowserRouter basename="/medicine-delivery-app">
+    <BrowserRouter>
      <Suspense fallback={<Loader />}>
       <App />
      </Suspense>
